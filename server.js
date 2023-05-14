bdd04035400e0ae49d2a7e82b57f4e5cacc6f46d
easakibahmad <sakib.sgc.du@gmail.com>
2023-05-15 02:26:15 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const next = require("next");
-
+const cors = require("cors");
 const port = parseInt(process.env.PORT, 10) || 5000;
 const dev = process.env.NODE_ENV !== "production";
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = next({ dev });
+
 const handle = app.getRequestHandler();
+// mongodb setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@firstcluster.3g9daa6.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+// mongodb setup
+// check db is connected or not
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+// check db is connected or not
 
 app.prepare().then(() => {
   const server = express();
+
+  // move middleware setup code here
+  server.use(cors());
+  server.use(express.json());
 
   server.get("*", (req, res) => {
     return handle(req, res);
